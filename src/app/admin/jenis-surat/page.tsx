da87e72f9fff0c5936d2f6b0cecec38860a0c834
Alfian Ade Faka <sysadmin@codesome.id>
2025-08-08 14:00:26 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- src/app/admin/jenis-surat/page.tsx
+++ src/app/admin/jenis-surat/page.tsx
@@ -23,6 +23,7 @@ import {
   AlertDialogFooter,
   AlertDialogHeader,
   AlertDialogTitle,
+  AlertDialogTrigger,
 } from "@/components/ui/alert-dialog";
 import { Input } from '@/components/ui/input';
 import { ScrollArea } from '@/components/ui/scroll-area';
